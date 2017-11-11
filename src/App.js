d9e3581a89cee31bd9b0cea50df3d0d823d7901d
warlyware <danward@gmail.com>
2017-11-10 22:09:19 -0500

    Add basic Spotify login flow

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SpotifyPlayer from './component/spotify-player.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <SpotifyPlayer />
       </div>
     );
   }
--- src/component/spotify-player.js
+++ src/component/spotify-player.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import * as SpotifyWebApi from 'spotify-web-api-js';
+import CLIENT_ID from '../client-id';
 
 const spotify = new SpotifyWebApi();
 
 export default class SpotifyPlayer extends Component {
 
-  componentDidMount() {
-    spotifyApi.setAccessToken('498db57ea6ac4a65b61f8bfc2fc392c3');
-    spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
-      if (err) console.error(err);
-      else console.log('Artist albums', data);
-    });
+  openSpotifyLogin(url) {
+    var width = 450,
+    height = 730,
+    left = (window.innerWidth / 2) - (width / 2),
+    top = (window.innerHeight / 2) - (height / 2);
+    window.open(url,
+      'Spotify',
+      'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+     );
+  }
+
+  async componentDidMount() {
+    const REDIRECT_URI = 'http://localhost:3000/';
+    const scopes = [
+      'user-read-email',
+    ];
+
+    const url = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}` +
+    `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
+    `&scope=${encodeURIComponent(scopes.join(' '))}&response_type=token`;
+
+    // this.openSpotifyLogin(url);
   }
 
   render() {
